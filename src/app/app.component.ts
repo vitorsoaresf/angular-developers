59936fbe61bbbc0376861ef6a3499e53e99ae0cb
Vitor Soares <vitorsoaresf@gmail.com>
2024-03-14 12:20:54 -0300

    feat: implements developer cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MainComponent } from './components/main/main.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, MainComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { UserCardComponent } from '../user-card/user-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, UserCardComponent],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+  styleUrl: './main.component.css',
 })
 export class MainComponent {
-
+  userList = [
+    {
+      name: 'Alex',
+      image: 'assets/image/josefine.jpg',
+      description: 'Lorem ipsum dolor sit amet',
+    },
+    {
+      name: 'Lucy',
+      image: 'assets/image/lucy.jpg',
+      description: 'Lorem ipsum dolor sit amet',
+    },
+    {
+      name: 'Robert',
+      image: 'assets/image/robert.jpg',
+      description: 'Lorem ipsum dolor sit amet',
+    },
+    {
+      name: 'Rodolf',
+      image: 'assets/image/rodolf.jpg',
+      description: 'Lorem ipsum dolor sit amet',
+    },
+  ];
 }
